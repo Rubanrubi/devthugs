1855b18a6b8a8e3c6212774ac12b5cdac5faff07
Rubanrubi <ruban@sparkouttech.com>
2021-06-06 00:19:13 +0530

    initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <!-- Required Meta Tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link rel="icon" type="image/png" href="{{asset('/img/favicon.png') }}"/>
+
+    <!--Core CSS -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&amp;display=swap" rel="stylesheet">
+    {{--        <link href="{{ mix('css/mapbox-gl.css')}}" rel="stylesheet" />--}}
+    <link rel="stylesheet" href="{{ mix('/css/all.css') }}" as="style">
+    {{--        <link id="theme-sheet" rel="stylesheet" href="{{ mix('assets/css/core.css')}}">--}}
+</head>
+<body class="is-theme-core">
+{{--<div class="pageloader"></div>--}}
+{{--<div class="infraloader is-active"></div>--}}
+<!-- Hero (Parallax) and nav -->
+<div class="hero is-app-grey">
+
+@include('includes.navbar')
+
+<!-- Hero image -->
+    <div id="main-hero" class="hero-body">
+        <div class="container has-text-centered">
+            <div class="columns is-vcentered">
+                <div class="column is-6 is-offset-3 has-text-centered is-subheader-caption">
+                    <h1 class="title is-2">
+                        BLOG
+                    </h1>
+                    <h2 class="subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quid enim? Immo videri
+                        fortasse. Compensabatur, inquit.</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@yield('content')
+
+@include('includes.footer')
+
+@include('includes.sidebar')
+
+@include('includes.chart-bot')
+
+<script async>
+    // Make sure sw are supported
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+            navigator.serviceWorker
+                .register('../sw_cached_pages.js')
+                .then(reg => console.log('Service Worker: Registered (Pages)'))
+                .catch(err => console.log(`Service Worker: Error: ${err}`));
+        });
+    }
+</script>
+
+<script src="{{ mix('/js/all.js') }}" as="script" async></script>
+
+{{--    <script src="assets/js/app.js"></script>--}}
+{{--    <script src="assets/js/core.js"></script>--}}
+</body>
+</html>
---
+++ webpack.mix.js
@@ -0,0 +1,37 @@
+const mix = require('laravel-mix');
+require('laravel-mix-purgecss');
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+// Template Js
+mix.scripts([
+    'resources/assets/js/app.js',
+    'resources/assets/js/core.js'], 'public/js/all.js')
+    .sourceMaps()
+
+    // Template Css
+    mix.styles([
+        'resources/assets/css/app.css',
+        'resources/assets/css/core.css'
+    ], 'public/css/all.css')
+
+    // Demo images
+    .copy('resources/assets/img/','public/img/')
+
+    // Template fonts
+    .copy('resources/assets/fonts/','public/fonts/')
+
+if (mix.inProduction()) {
+    mix.version()
+   .purgeCss();
+}
+
+
