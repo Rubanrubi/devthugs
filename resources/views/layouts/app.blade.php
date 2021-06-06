4f444386f7da059aabbbb081ee8510c0827e6836
Rubanrubi <ruban@sparkouttech.com>
2021-06-06 12:01:33 +0530

    manifiest fetching

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,7 +51,7 @@
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
             navigator.serviceWorker
-                .register('../sw_cached_pages.js')
+                .register(window.location.origin +'/sw_cached_pages.js')
                 .then(reg => console.log('Service Worker: Registered (Pages)'))
                 .catch(err => console.log(`Service Worker: Error: ${err}`));
         });
